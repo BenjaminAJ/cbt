aaca0f5775e62e18dde09313a964b0787f5047d4
BenjaminAJ <ajanakub30@gmail.com>
2023-04-07 09:24:58 +0100

    made modifications to start and submit functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const timer = document.getElementById("timer");
 const timeup = document.getElementById("timeup");
 const startBtn = document.getElementById("startBtn");
+const questionCont = document.getElementById("questionContainer");
+const resultCont = document.getElementById("resultContainer");
 let timeobj = { sec: "" };
 let questionobj = { 
     question: "",
@@ -8,13 +10,42 @@ let questionobj = {
     options :[],
  };
 let timerinterval;
+let score;
 
 function start(params) {
   startCountdown();
   startBtn.style.display = 'none';
+  questionCont.innerHTML = `
+    <div class="row"><h3>Question</h3></div>
+    <div class="row">
+      <div class="col-auto">
+        <button class="btn btn-secondary" onclick="previous()">Previous</button>
+      </div>
+      <div class="col-auto">
+        <button class="btn btn-secondary" onclick="next()">Next</button>
+      </div>
+      <div class="col-auto">
+        <button class="btn btn-secondary" onclick="submit()">Submit</button>
+      </div>
+    </div>
+  `;
 }
 function submit(params) {
   stopCountdown();
+  questionCont.style.display = 'none';
+  if (score > 50) {
+    resultCont.innerHTML =`
+    <div class="row"><h3>Congratulations!!!</h3></div>
+  
+    `;
+  }
+  else{
+    resultCont.innerHTML =`
+    <div class="row"><h3>Better luck next time.</h3></div>
+  
+    `;
+  
+  }
 }
 function startCountdown(params) {
   timeobj.sec = timer.value;
